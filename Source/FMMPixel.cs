3cc9c92f71dc38543391e9d2734752c9f330795e
Maxime Boucher <mxboucher@gmail.com>
2021-04-02 10:23:14 +0200

    sortedSet (comparer pour sortedSet)

--- Source/FMMPixel.cs
+++ Source/FMMPixel.cs
@@ -1,14 +1,16 @@
-﻿namespace m1_image_projet.Source
+﻿using System.Collections.Generic;
+
+namespace m1_image_projet.Source
 {
     public sealed partial class Inpainting
     {
         public class FMMPixel
         {
-            private float T; // value
-            private float I; // gray value
-            private flag f; // flag
+            public float T; // value
+            public float I; // gray value
+            public flag f; // flag
 
-            private enum flag
+            public enum flag
             {
                 BAND,
                 KNOWN,
@@ -16,6 +18,35 @@
             }
 
         }
+
+        public class ByTValues : IComparer<FMMPixel>
+        {
+            public int Compare(FMMPixel x, FMMPixel y)
+            {
+                if (x.T == y.T) {
+                    return 0;
+                } else if (x.T < y.T) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        }
+
+        public class ByIValues : IComparer<FMMPixel>
+        {
+            public int Compare(FMMPixel x, FMMPixel y)
+            {
+                if (x.I == y.I) {
+                    return 0;
+                } else if (x.I < y.I) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        }
+
     }
 
 }
